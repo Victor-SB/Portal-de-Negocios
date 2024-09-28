72e4d9872b928902155d65f1ab260fec23538d61
Victor Hugo <133723528+Victor-SB@users.noreply.github.com>
2024-09-28 10:41:55 -0300

    Projeto Criado

---
+++ PortalDeNegocios/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalDeNegocios.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalDeNegocios.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult About() {
+            ViewData["Message"] = "Portal de Negocios - NWGroup";
+            ViewData["Desenvolvedor"] = "Victor Hugo";
+
+            return View();
+        }
+
+        public IActionResult Contact() {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
