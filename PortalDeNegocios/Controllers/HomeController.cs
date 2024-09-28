951f6f53fdd044ca257f87cb7c6a56faca2bc369
Victor Hugo <133723528+Victor-SB@users.noreply.github.com>
2024-09-28 12:53:02 -0300

    First Model-Controller-View - Department

--- PortalDeNegocios/Controllers/HomeController.cs
+++ PortalDeNegocios/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using PortalDeNegocios.Models.ViewModels;
 
 namespace PortalDeNegocios.Controllers {
     public class HomeController : Controller {
